a5dc9f5996bfd74432e5596b46b858e4f03d8db1
Sarankhuu Batsaikhan <khuu.saran@gmail.com>
2026-05-25 21:00:28 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.9
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "String"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))

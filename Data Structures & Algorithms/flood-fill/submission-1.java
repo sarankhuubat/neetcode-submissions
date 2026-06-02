a0594bf2ad70f0ef06d9e8cba46e7679b3fc736a
Sarankhuu Batsaikhan <khuu.saran@gmail.com>
2026-06-02 13:52:00 -0500

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+    private int[][] dirs = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        return dfs(image, sr, sc, color, image[sr][sc]);
+    }
+
+    private int[][] dfs(int[][] image, int r, int c, int color, int original){
+        if(r < 0 || c < 0 || r >= image.length || c >= image[0].length || original == color ||image[r][c] != original)
+            return image;
+
+        //if(image[r][c] == original){
+            image[r][c] = color;
+            for(int[] dir : dirs){
+                dfs(image, r + dir[0], c + dir[1], color, original);
+            }
+        //}
+
+        return image;
+    }
+}
+/*
+[[1,1,1],
+ [1,1,0],
+ [1,0,1]]
+
+[[1,1,1],
+ [1,2,0],
+ [1,0,1]]
+
+[[1,2,1],
+ [2,2,0],
+ [1,0,1]]
+
+[[2,2,2],
+ [2,2,0],
+ [2,0,1]]
+*/

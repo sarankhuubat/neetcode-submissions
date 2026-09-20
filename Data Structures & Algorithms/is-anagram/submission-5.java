fd206cab6239bd7adc93a75d141c70c338f76b8d
Sarankhuu Batsaikhan <khuu.saran@gmail.com>
2026-09-20 11:48:57 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> sCount = new HashMap<>();
+        HashMap<Character, Integer> tCount = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            sCount.put(c, sCount.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : t.toCharArray()){
+            tCount.put(c, tCount.getOrDefault(c, 0) + 1);
+        }
+
+        return sCount.equals(tCount);
+    }
+}
